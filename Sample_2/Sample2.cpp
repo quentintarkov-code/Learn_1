71ce39259fdcc5a91c3cf8d1553cdec942c8cc82
quentintarkov-code <quentintarkov@gmail.com>
2026-01-06 13:14:36 +0700

    Add area calculation to Sample2.cpp

--- Sample_2/Sample2.cpp
+++ Sample_2/Sample2.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 
 int main() {
-    int length;
+    int length, width;
     length = 5;
+    width = 10;
     cout << "Length is: " << length << endl;
+    cout << "Width is: " << width << endl;
+    cout << "Area is: " << length * width << endl;
     return 0;
 }
